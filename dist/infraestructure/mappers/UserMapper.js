18f0998a5dede249fb50feec2d6af39bafcb16c3
Willy N <tenantz200@gmail.com>
2024-11-08 13:41:24 -0500

    feat: adding .gitignore

--- dist/infraestructure/mappers/UserMapper.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UserMapper = void 0;
-class UserMapper {
-}
-exports.UserMapper = UserMapper;
--- dist/infraestructure/middlewares/async.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler_1 = require("./errorHandler");
-const asyncMiddleware = (fn) => (req, res, next) => {
-    fn(req, res, next).catch((error) => {
-        return (0, errorHandler_1.errorHandler)(error, req, res);
-    });
-};
-exports.default = asyncMiddleware;
